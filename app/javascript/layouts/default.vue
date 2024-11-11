c410d646d4f53a99c893004aef81d594b633b113
Delton Ding <shenghao.ding@yetanother.ai>
2024-11-11 10:16:11 +0800

    stash new ui

--- app/javascript/layouts/default.vue
+++ app/javascript/layouts/default.vue
@@ -1,11 +1,10 @@
 <template lang="pug">
 t-layout
   t-header
-    t-head-menu(v-model="pathname", @change="switchRoute")
+    t-head-menu
       template(#logo)
-        h2 Lerp
-      t-menu-item(:value="root_path()", :router="{}")
-        | Home
+        i-link.logo(:href="root_path()")
+          h2 Lerp
       template(#operations)
         t-button(variant="text", shape="square")
           template(#icon)
@@ -17,10 +16,23 @@ t-layout
         t-button(variant="text", shape="square", @click="logout")
           template(#icon)
             t-icon(name="logout")
-  t-layout.content
-    slot
-  t-footer
-    | © {{ new Date().getFullYear() }} Yet Another AI Ltd.
+  t-layout
+    t-aside.sidebar
+      t-menu(v-model="pathname", @change="switchRoute", :collapsed="collapsed")
+        t-menu-item(:value="root_path()", :router="{}")
+          template(#icon)
+            t-icon(name="app")
+          | Home
+        template(#operations)
+          t-button(variant="text", shape="square", @click="collapsed = !collapsed")
+            template(#icon)
+              t-icon(v-if="collapsed", name="chevron-right")
+              t-icon(v-else, name="chevron-left")
+    t-layout
+      t-content.content
+        slot
+      t-footer
+        | © {{ new Date().getFullYear() }} Yet Another AI Ltd.
 </template>
 
 <script lang="ts" setup>
@@ -37,15 +49,12 @@ const props = defineProps<{
   };
 }>();
 
+const collapsed = ref(true);
 const pathname = ref(window.location.pathname);
 
-router.on('navigate', (event) => {
-  pathname.value = event.detail.page.url;
-});
+router.on('navigate', (event) => pathname.value = event.detail.page.url);
 
-const switchRoute = (value: string) => {
-  router.visit(value);
-};
+const switchRoute = (value: string) => router.visit(value);
 
 const logout = () => submitForm('delete', sessions_path(), {});
 
@@ -56,6 +65,13 @@ onMounted(() => {
 </script>
 
 <style lang="stylus" scoped>
+.logo
+  text-decoration none
+  color inherit
+
 .content
-  min-height calc(100vh - 120px)
+  min-height calc(100vh - 126px)
+
+.sidebar
+  width auto
 </style>
